06ffc89973f2fe42bda61c901178356bb0a23a25
Christoph <christoph-85@hotmail.com>
2019-11-08 08:40:20 +0800

    added basic style of scanning screen

--- cra-pos-poc/src/Views/ScanningScreen.js
+++ cra-pos-poc/src/Views/ScanningScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext} from 'react'
 import { useGlobalState } from "../States/GlobalState"
+import './ScanningScreen.css';
 
 //ACTIONS
 import { currentScreen } from '../Actions/appSettings'
@@ -13,8 +14,49 @@ export default function ScanningScreen() {
     }
 
     return (
-        <div onClick={changeScreenAction}>
-           scanning 
+        <div className="scanningScreen-Container">
+            
+            <div className="scannedItem-Display">
+                
+                <div className="useScanner-Box">
+                    <div className="scannedItem-Title">
+                        Use Scanner to add items to your cart
+                    </div>
+                </div>
+
+                <div className="information-Box"></div>
+
+                <div className="supportButtons-Container">
+                        <div className="TEMP-SupportButton">
+                            <div className="TEMP-Icon"></div>
+                            <div>English</div>
+                        </div>  
+
+                        <div className="TEMP-SupportButton">
+                            <div className="TEMP-Icon"></div>
+                            <div>Accessibility</div>
+                        </div>                  
+                </div>
+
+            
+                
+            </div>
+
+            <div className="myCart-Container">
+                
+                <div className="MyCart-Toolbar">
+                    <div style={{height:'20px', width: '20px', color: '#DA291C'}}></div>
+                </div>
+                
+                {/* just to allow flicking between screens */}
+                <div onClick={changeScreenAction}>
+                scanning 
+                </div>
+
+            </div>
+            
+
+
         </div>
     )
 }
